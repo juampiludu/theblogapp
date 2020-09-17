5136da8b75a2c10ca5e711a316ee10e6ff2cdecf
juampiludu <juanzakka@gmail.com>
2020-09-17 17:53:50 -0300

    some changes

--- Blog/config/urls.py
+++ Blog/config/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('site/administration/', admin.site.urls),
     path('api/post/', include('post.urls')),
     path('api/account/', include('account.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
